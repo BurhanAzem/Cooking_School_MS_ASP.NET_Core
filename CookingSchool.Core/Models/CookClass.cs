98341cbbb4f205ce3227d01069217d0c63cbba1e
BurhanAzem <104472357+BurhanAzem@users.noreply.github.com>
2023-06-15 10:09:40 +0300

    Update CookClass.cs

--- CookingSchool.Core/Models/CookClass.cs
+++ CookingSchool.Core/Models/CookClass.cs
@@ -7,16 +7,16 @@ namespace Cooking_School.Core.Models
     {
         [ForeignKey(nameof(Course))]
         public int CourseId { get; set; }
-        public virtual Course Course { get; set; }
+        public Course Course { get; set; }
 
         [ForeignKey (nameof(Chef))]
         public int ChefId { get; set; } 
-        public virtual Chef Chef { get; set; }
+        public Chef Chef { get; set; }
         public TimeOnly StartingAt { get; set; }
         public TimeOnly EndingAt { get; set; }
-        public virtual ICollection<Project> Projects { get; set; }
-        public virtual ICollection<ApplicationT> Applications { get; set; }
-        public virtual ICollection<ClassDays> ClassDays { get; set; }
+        public ICollection<Project> Projects { get; set; }
+        public ICollection<ApplicationT> Applications { get; set; }
+        public ICollection<ClassDays> ClassDays { get; set; }
 
     }
 }
